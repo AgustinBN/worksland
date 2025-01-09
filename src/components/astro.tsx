936f5d465a1eb59fc77ce8d5d9c1cb108368cd92
Agustin Bianchi <bianchiagus6@gmail.com>
2025-01-08 22:45:15 -0300

    add framer-motion & astro animation

--- src/components/astro.tsx
+++ src/components/astro.tsx
@@ -1,19 +1,35 @@
+"use client";
+
+import { motion } from "framer-motion";
 import Image from "next/image";
 import AstroImg from "../../public/astro.png";
 
 const Astro = () => {
   return (
-    <div className="flex items-center justify-center mt-5  pt-[70px]">
-      <Image
-        src={AstroImg}
-        alt="Astronaut"
-        style={{
-          width: "200px",
-          height: "200px",
-          filter: "drop-shadow(5px 5px 15px #6B46C1)",
-          opacity: "0.8",
+    <div className="w-full flex justify-center items-center pt-[70px]">
+      <motion.div
+        animate={{
+          y: [0, -10, 0, 10, 0], // Moves up and down
+          rotate: [0, -5, 0, 5, 0], // Slight rotation
         }}
-      />
+        transition={{
+          duration: 4,
+          ease: "easeInOut",
+          repeat: Infinity, // Continuous loop
+          repeatType: "loop",
+        }}
+      >
+        <Image
+          src={AstroImg}
+          alt="Astronaut"
+          style={{
+            width: "200px",
+            height: "200px",
+            filter: "drop-shadow(5px 5px 15px #6B46C1)",
+            opacity: "0.8",
+          }}
+        />
+      </motion.div>
     </div>
   );
 };
